96b85f27db8ed9bd357fcdcb8c7da60b1dd5c421
Kevin <kchow_21@hotmail.com>
2016-01-06 10:52:14 -0800

    Added winner function and simple check to see who wins by using $_GET

--- src/index.php
+++ src/index.php
@@ -12,18 +12,60 @@
      * Date: 1/6/2016
      * Time: 10:06 AM
      */
-    $temp = 'Kevin';
-    $var = 'See?';
-    echo 'Hello there, I am '. $temp. ' and I know how to use variables! '. $var.PHP_EOL;
-    $rate = 15;
-    $hours = $_GET['hours'];
-    if($hours > 40){
-        $total = $hours * $rate * 1.5;
+    if(! isset($_GET['board'])){
+        echo "No board found";
     }
-    else {
-        $total = $hours * $rate;
+    else{
+        $board = $_GET['board'];
+        $squares = str_split($board);
+        if(winner('x', $squares)){
+            echo "You win!";
+        }
+        elseif(winner('o', $squares)){
+            echo "I win!";
+        }
+        else{
+            echo "No winner yet :(";
+        }
     }
-    echo ($total > 0)? 'Pay me '.$total : "You're welcome";
     ?>
     </body>
 </html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 1/6/2016
+ * Time: 10:06 AM
+ */
+
+function winner($token, $position) {
+    $won = false;
+    if( ($position[0] == $token) && ($position[1] == $token) && ($position[2] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[3] == $token) && ($position[4] == $token) && ($position[5] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[6] == $token) && ($position[7] == $token) && ($position[8] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[0] == $token) && ($position[3] == $token) && ($position[6] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[2] == $token) && ($position[5] == $token) && ($position[8] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[1] == $token) && ($position[4] == $token) && ($position[7] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token) ){
+        $won = true;
+    }
+    return $won;
+}
+?>
