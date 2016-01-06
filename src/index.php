5045783d8ea1f813765ff5ac7f0e05f33d145d7b
Kevin <kchow_21@hotmail.com>
2016-01-06 10:11:38 -0800

    index.php hello world file

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+    <?php
+        echo 'Hello World';
+    ?>
+    </body>
+</html>
+/**
+ * Created by PhpStorm.
+ * User: Kevin
+ * Date: 1/6/2016
+ * Time: 10:06 AM
+ */
