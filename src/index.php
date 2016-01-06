2e40985eca5fb325c102d916c0e0d862ad13ff85
Kevin <kchow_21@hotmail.com>
2016-01-06 10:21:12 -0800

    Changed index.php to use dot for concat and fixed missing html tag

--- src/index.php
+++ src/index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
+<html>
     <head>
         <meta http-equiv="content-type" content="text/html; charset=UTF-8">
         <title></title>
     </head>
     <body>
     <?php
-        echo 'Hello World';
+    /**
+     * Created by PhpStorm.
+     * User: Kevin
+     * Date: 1/6/2016
+     * Time: 10:06 AM
+     */
+        $temp = 'Kevin';
+        $var = 'See?';
+        echo 'Hello there, I am '. $temp. ' and I know how to use variables! '. $var;
     ?>
     </body>
 </html>
-/**
- * Created by PhpStorm.
- * User: Kevin
- * Date: 1/6/2016
- * Time: 10:06 AM
- */
