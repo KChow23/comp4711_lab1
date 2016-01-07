75d12ec483ce801191a6c83994b1e51e252420f0
Kevin <kchow_21@hotmail.com>
2016-01-06 21:21:36 -0800

    Added game class and moved winner function

--- src/index.php
+++ src/index.php
@@ -16,16 +16,16 @@
         echo "No board found";
     }
     else{
-        $board = $_GET['board'];
-        $squares = str_split($board);
-        if(winner('x', $squares)){
-            echo "You win!";
+        $squares = str_split($_GET['board']);
+        $game = new Game($squares);
+        if($game->winner('x')){
+            echo 'You win. Lucky guesses';
         }
-        elseif(winner('o', $squares)){
-            echo "I win!";
+        elseif($game->winner('o')){
+            echo 'I win!';
         }
         else{
-            echo "No winner yet :(";
+            echo "No winner yet.";
         }
     }
     ?>
@@ -40,51 +40,4 @@
  * Time: 10:06 AM
  */
 
-function winner($token, $position) {
-
-    $won = false;
-
-    for($row=0; $row<3; $row++){
-        //echo 'ROW VALUE: '. $row.PHP_EOL;
-        $won = true;
-        for($col=0; $col<3; $col++){
-           // echo "COL VALUE: " . (3*$row+$col).PHP_EOL;
-            echo "INDEX: ". (3*$row+$col);
-            if($position[3*$row+$col] != $token){
-                $won = false;
-                break;
-            }
-        }
-        if($won){
-            return $won;
-        }
-
-    }
-    for($row=0; $row<3; $row++){
-        //echo 'ROW VALUE: '. $row.PHP_EOL;
-        $won = true;
-        for($col=0; $col<3; $col++){
-            // echo "COL VALUE: " . (3*$row+$col).PHP_EOL;
-            echo "INDEX: ". (3*$col+$row);
-            if($position[3*$col+$row] != $token){
-                $won = false;
-                break;
-            }
-        }
-        if($won){
-            return $won;
-        }
-
-    }
-
-    if( ($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token) ){
-        $won = true;
-    }
-
-    return $won;
-
-}
 ?>
