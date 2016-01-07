2ec85c35d5b146a68f05fd8bb0f66fa6289abf1c
Kevin <kchow_21@hotmail.com>
2016-01-06 20:55:46 -0800

    Fixed winner function to check all win states.

--- src/index.php
+++ src/index.php
@@ -41,47 +41,16 @@
  */
 
 function winner($token, $position) {
-    /*
-    if( ($position[0] == $token) && ($position[1] == $token) && ($position[2] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[3] == $token) && ($position[4] == $token) && ($position[5] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[6] == $token) && ($position[7] == $token) && ($position[8] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[0] == $token) && ($position[3] == $token) && ($position[6] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[2] == $token) && ($position[5] == $token) && ($position[8] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[1] == $token) && ($position[4] == $token) && ($position[7] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token) ){
-        $won = true;
-    }
-    elseif( ($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token) ){
-        $won = true;
-    }*/
-    /*
-    for($row=0; $row < 3; $row++){
-        if( ($position[3*$row] == $token) && ($position[3*$row+1] == $token) && ($position[3*$row+2] == $token)){
-            $won = true;
 
-        }
-    }*/
     $won = false;
+
     for($row=0; $row<3; $row++){
-        echo 'ROW VALUE: '. $row.PHP_EOL;
+        //echo 'ROW VALUE: '. $row.PHP_EOL;
         $won = true;
         for($col=0; $col<3; $col++){
-            echo "COL VALUE: " . (3*$row+$col).PHP_EOL;
+           // echo "COL VALUE: " . (3*$row+$col).PHP_EOL;
             echo "INDEX: ". (3*$row+$col);
             if($position[3*$row+$col] != $token){
-                echo "THIS DOESN'T WORK!";
                 $won = false;
                 break;
             }
@@ -91,6 +60,30 @@ function winner($token, $position) {
         }
 
     }
+    for($row=0; $row<3; $row++){
+        //echo 'ROW VALUE: '. $row.PHP_EOL;
+        $won = true;
+        for($col=0; $col<3; $col++){
+            // echo "COL VALUE: " . (3*$row+$col).PHP_EOL;
+            echo "INDEX: ". (3*$col+$row);
+            if($position[3*$col+$row] != $token){
+                $won = false;
+                break;
+            }
+        }
+        if($won){
+            return $won;
+        }
+
+    }
+
+    if( ($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token) ){
+        $won = true;
+    }
+    elseif( ($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token) ){
+        $won = true;
+    }
+
     return $won;
 
 }
